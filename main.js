7405fd76f7d3d098e9edbde0d6c786303ff7e9f7
igormatheus2309 <111667359+igormatheus2309@users.noreply.github.com>
2022-08-20 22:12:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,87 @@
+const cardBoard = document.querySelector("#cardboard");
+const imagens = [
+
+'coelhinha.png',
+'patolinopernalonga.png',
+'patolino.png',
+'pernalonga.png',
+'piupiu.png',
+'taz.png'
+
+];
+
+let cardHTML = '';
+imagens.forEach(img => {
+    cardHTML += `
+    <div class="memory-card" data-card="${img}">
+        <img class="front-face" src="img/${img}">
+        <img class="back-face" src="img/LooneyTunes.png">
+    
+    </div>
+    `;
+});
+
+
+cardBoard.innerHTML = cardHTML + cardHTML;
+
+// Final do Html...
+
+
+const cards = document.querySelectorAll('.memory-card');
+let firstCard, secondCard;
+let lockCard = false;
+
+function flipcard(){
+
+if(lockCard) return false;
+this.classList.add("flip");
+
+if(!firstCard){
+     firstCard = this;
+    return false;
+  }
+
+  secondCard = this;
+checkForMatch();
+}
+
+function checkForMatch(){
+
+    let isMatch = firstCard.dataset.card === secondCard.dataset.card;
+
+    !isMatch ? disableCards(): resetCards(isMatch);
+
+}
+
+function disableCards(){
+lockCard = true;
+
+setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+
+    resetCards();
+}, 1000);
+}
+
+(function shuffle(){
+ cards.forEach(card => {
+  let rand =Math.floor(Math.random() * 12);
+  card.style.order = rand
+ })
+
+})()
+
+function resetCards(isMatch = false){
+    if(isMatch){
+        firstCard.removeEventListener('click', flipcard);
+        secondCard.removeEventListener('click', flipcard);
+    }
+    
+    [firstCard, secondCard, lockCard] = [null, null ,false]
+
+
+}
+
+cards.forEach(card => card.addEventListener('click',flipcard));
+ 
